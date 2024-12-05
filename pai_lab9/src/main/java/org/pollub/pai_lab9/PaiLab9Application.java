0710e4a9f75d36d2895afd37474f1ffaa62a1f5a
Dawid Grabek <grabekd37@gmail.com>
2024-12-05 22:53:58 +0100

    finish lab 9

---
+++ pai_lab9/src/main/java/org/pollub/pai_lab9/PaiLab9Application.java
@@ -0,0 +1,13 @@
+package org.pollub.pai_lab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiLab9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiLab9Application.class, args);
+	}
+
+}
---
+++ pai_lab9/src/test/java/org/pollub/pai_lab9/PaiLab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pollub.pai_lab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiLab9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
